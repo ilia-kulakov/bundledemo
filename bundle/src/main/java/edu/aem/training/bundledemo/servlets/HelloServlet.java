c1fa691c5f661d8c16884201fea3be1180eb2562
veterkam <veterkam@gmail.com>
2019-01-21 14:20:42 +0300

    Initialization

--- bundle/src/main/java/edu/aem/training/bundledemo/servlets/HelloServlet.java
+++ bundle/src/main/java/edu/aem/training/bundledemo/servlets/HelloServlet.java
@@ -1,10 +1,12 @@
 package edu.aem.training.bundledemo.servlets;
 
 import edu.aem.training.bundledemo.HelloService;
+import edu.aem.training.bundledemo.models.HelloModel;
 import org.apache.felix.scr.annotations.Reference;
 import org.apache.felix.scr.annotations.sling.SlingServlet;
 import org.apache.sling.api.SlingHttpServletRequest;
 import org.apache.sling.api.SlingHttpServletResponse;
+import org.apache.sling.api.resource.Resource;
 import org.apache.sling.api.servlets.SlingSafeMethodsServlet;
 
 import javax.servlet.ServletException;
@@ -26,8 +28,8 @@ public class HelloServlet extends SlingSafeMethodsServlet {
         response.setStatus(HttpServletResponse.SC_OK);
         response.setContentType("text/html");
 
-//        Resource r = request.getResource();
-//        HelloModel hm = r.adaptTo(HelloModel.class);
+        Resource r = request.getResource();
+        HelloModel hm = r.adaptTo(HelloModel.class);
 
         PrintWriter writer = response.getWriter();
         writer.write("<p>"+ helloService.getGreeting() + "</p>");
